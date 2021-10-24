2d66d992807c483c2e75f1e6dfca0ba30ed89528
KliffUndersn <kliffundersn90@gmail.com>
2021-10-24 20:51:56 +0300

    done?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
-import InputForm from "./components/InputForm/InputForm";
+import React from 'react';
+import InputForm from './components/InputForm/InputForm';
+import store from './components/redux/store';
+import { Provider } from 'react-redux';
+
+store.getState();
 
 function App() {
-  return <InputForm />;
+  return (
+    <>
+      <Provider store={store}>
+        <InputForm />
+      </Provider>
+    </>
+  );
 }
 
 export default App;
--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -2,85 +2,79 @@ import { useState } from 'react';
 import { v4 as generate } from 'uuid';
 import { ContactList } from './ContactList/ContactList';
 import { FilterContacts } from './FilterContacts/FilterContacts';
-import useLocalStorage from '../../hooks/useLocalStorage'
+import { connect } from 'react-redux';
+import { addContact, deleteContact, filterContact } from '../redux/actions';
 
-const InputForm =() => {
-
-
- const [contacts, setContacts] = useLocalStorage('contacts', '');
- const [state,setState] = useState({
+const InputForm = ({ onSubmit, items, onDelete, filter, changiFilter }) => {
+  const [state, setState] = useState({
     name: '',
-    filter: '',
-    number: '',    
-  })
+    number: '',
+  });
   const handleChange = ({ target }) => {
-    setState({...state,
-      [target.name]: target.value,
-    });
-   
+    setState({ ...state, [target.name]: target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const {name, number} = state
-    if (contacts.length === 0) {return setContacts([{id: generate(),
-      name,
-      number,}])}
-    const newName = contacts.map(e=> e.name)
-    if (newName.includes(e.target.name.value)){return alert(`${e.target.name.value} says hello from chat`)}
+    const { name, number } = state;
+    const newName = items.map(e => e.name);
+    if (newName.includes(e.target.name.value)) {
+      return alert(`${e.target.name.value} says hello from chat`);
+    }
     const singleContact = {
       id: generate(),
       name,
       number,
-    }
-    setContacts([...contacts, singleContact])
-    
-  }
-  const filterContacts = (e) => {
-    if (contacts.length === 0) {return}
-    return contacts.filter((e) => e.name.toLowerCase().includes(state.filter))
-  }
+    };
+    onSubmit(singleContact);
+  };
+  const filterContacts = e => {
+    return items.filter(e => e.name.toLowerCase().includes(filter));
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Phonebook</h1>
+        <h2>Name</h2>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          onChange={handleChange}
+        />
+        <h2>Number</h2>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit">Add contact</button>
+      </form>
 
-  const changiFilter = ({target}) => {
-  
-    setState({...state, filter:target.value})
-  }
-  const deleteContact = (id) => {
-   setContacts(contacts.filter(e=>e.id !==id))
-  }
+      <FilterContacts filtered={changiFilter} />
 
-    return (<>
-        <form onSubmit={handleSubmit}>
-          <h1>Phonebook</h1>
-          <h2>Name</h2>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            onChange={handleChange}
-          />
-          <h2>Number</h2>
-         <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-            onChange={handleChange}
-          />
-          <button type="submit">Add contact</button>
-          </form>
-          
-          <FilterContacts filtered={changiFilter} />
-          
-          
-          <ContactList filteredContacts={filterContacts()} deleteContact={deleteContact}/>
-        </>
+      <ContactList
+        filteredContacts={filterContacts()}
+        deleteContact={onDelete}
+      />
+    </>
+  );
+};
 
-    )
-  
-}
-
-export default InputForm
+const mapStateToProps = ({ items, filter }) => {
+  return {
+    items,
+    filter,
+  };
+};
+const mapDispatchToProps = dispatch => ({
+  onSubmit: value => dispatch(addContact(value)),
+  onDelete: id => dispatch(deleteContact(id)),
+  changiFilter: ({ target }) => dispatch(filterContact(target.value)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(InputForm);
